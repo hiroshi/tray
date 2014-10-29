25af767beae43124de317c55dd64ad8df74348f2
Hiroshi Saito <hiroshi3110@gmail.com>
2014-10-29 13:52:32 +0900

    Add item from web.

--- app.js
+++ app.js
@@ -1,48 +1,24 @@
 /** @jsx React.DOM */
 var TrayStore = {
   dropbox: {},
-  // init: function() {
-  //   var client = new Dropbox.Client({key: DROPBOX_APP_KEY});
-  //   this.dropbox.client = client;
-  //   // Try to finish OAuth authorization.
-  //   client.authenticate({interactive: false}, function (error) {
-  //     if (error) {
-  //       alert('Authentication error: ' + error);
-  //     }
-  //   });
-  //   if (client.isAuthenticated()) {
-  //     // Client is authenticated. Display UI.
-  //     console.log("dropbox authenticated.")
-  //     requestSafariPush(function(deviceToken) {
-  //       var xhr = new XMLHttpRequest();
-  //       var payload = JSON.stringify({uid: client.dropboxUid(), deviceToken: deviceToken, accessToken: client.credentials().token});
-  //       console.log("payload: " + payload);
-  //       xhr.open('POST', TRAY_REGISTER_URL, true);
-  //       xhr.setRequestHeader('X-PINGOTHER', 'pingpong');
-  //       //xhr.setRequestHeader('Content-Type', "application/json");
-  //       xhr.send(payload);
-  //     });
-  //   }
-  //   window.dropboxLogin = function() {
-  //     client.authenticate();
-  //   };
-  // },
-  // promiseItems: function() {
-  //   var datastoreManager = this.dropbox.client.getDatastoreManager();
-  //   return new Promise(function(resolve, reject) {
-  //     datastoreManager.openDefaultDatastore(function(error, datastore) {
-  //       if (error) {
-  //         reject(error);
-  //       } else {
-  //         var itemTable = datastore.getTable('items');
-  //         var items = itemTable.query().map(function(record){ return record.getFields(); });
-  //         resolve(items.sort(function(a, b) {
-  //           return b.orderDate - a.orderDate;
-  //         }));
-  //       }
-  //     });
-  //   });
-  // },
+  _defaultDatastore: function() {
+    if (this.dropbox.defaultDatastore) {
+      return Bacon.once(this.dropbox.defaultDatastore);
+    } else {
+      return Bacon.fromCallback(function(callback) {
+        var datastoreManager = this.dropbox.client.getDatastoreManager();
+        datastoreManager.openDefaultDatastore(function(error, datastore) {
+          if (error) {
+            console.error(error);
+            callback(Bacon.Error(error));
+          } else {
+            this.dropbox.defaultDatastore = datastore;
+            callback(datastore);
+          }
+        }.bind(this));
+      }.bind(this))
+    }
+  },
   streamItems: function() {
     var client = new Dropbox.Client({key: DROPBOX_APP_KEY});
     this.dropbox.client = client;
@@ -58,7 +34,7 @@ var TrayStore = {
       requestSafariPush(function(deviceToken) {
         var xhr = new XMLHttpRequest();
         var payload = JSON.stringify({uid: client.dropboxUid(), deviceToken: deviceToken, accessToken: client.credentials().token});
-        console.log("payload: " + payload);
+        //console.log("payload: " + payload);
         xhr.open('POST', TRAY_REGISTER_URL, true);
         xhr.setRequestHeader('X-PINGOTHER', 'pingpong');
         //xhr.setRequestHeader('Content-Type', "application/json");
@@ -68,23 +44,37 @@ var TrayStore = {
     window.dropboxLogin = function() {
       client.authenticate();
     };
-    return Bacon.fromCallback(function(callback) {
-      if (this.dropbox.client.isAuthenticated()) {
-        var datastoreManager = this.dropbox.client.getDatastoreManager();
-        datastoreManager.openDefaultDatastore(function(error, datastore) {
-          if (error) {
-            console.error(error);
-            callback(Bacon.Error(error));
-          } else {
-            var itemTable = datastore.getTable('items');
-            var items = itemTable.query().map(function(record){ return record.getFields(); });
-            callback(items.sort(function(a, b) {
-              return b.orderDate - a.orderDate;
-            }));
+    return Bacon.fromBinder(function(callback) {
+      this._defaultDatastore().onValue(function(datastore) {
+        var callbackItems = function() {
+          var itemTable = datastore.getTable('items');
+          // Return initial items
+          var items = itemTable.query();
+          callback(items.sort(function(a, b) {
+            return b.get('orderDate') - a.get('orderDate');
+          }));
+        };
+        callbackItems();
+        // Listen to farther changes
+        datastore.recordsChanged.addListener(function(event) {
+          if (event.affectedRecordsForTable('items')) {
+            callbackItems();
           }
         });
+      });
+      return function() {
+        // unsub functionality here, this one's a no-op
+        console.log('unsub items stream');
       }
     }.bind(this));
+  },
+  addItem: function(text) {
+    this._defaultDatastore()
+    .onValue(function(datastore) {
+      var itemTable = datastore.getTable('items');
+      var now = new Date();
+      itemTable.insert({text: text, createDate: now, orderDate: now});
+    });
   },
 };
 
@@ -92,31 +82,45 @@ var TrayStore = {
 var App = React.createClass({
   getInitialState: function() {
     return {
-      items: []
+      login: false,
+      items: [],
     };
   },
   componentWillMount: function() {
-    TrayStore.streamItems()
-    .onValue(function(items) {
-      this.setState({items: items});
+    var stream = TrayStore.streamItems();
+    stream.onValue(function(items) {
+      console.log(items.length);
+      this.setState({items: items, login: true});
     }.bind(this));
-    // TrayStore.init();
-    // TrayStore.promiseItems()
-    // .then(function(items) {
-    //   this.setState({items: items});
-    // }.bind(this));
+    stream.onError(function(error) {
+      console.error(error);
+    });
+  },
+  _pushItem: function(e) {
+    e.preventDefault();
+    var text = this.refs.text.getDOMNode().value.trim();
+    TrayStore.addItem(text);
+    this.refs.text.getDOMNode().value = "";
   },
   render: function() {
-    var items = this.state.items.map(function(item) {
-      return <li>{item.text}</li>;
-    });
-    return (
-      <div>
-        <ul>
-          {items}
-        </ul>
-      </div>
-    );
+    if (this.state.login) {
+      items = this.state.items.map(function(item) {
+        return <li key={item.getId()}>{item.get('text')}</li>;
+      });
+      return (
+        <div>
+          <form onSubmit={this._pushItem}>
+            <textarea ref='text' />
+            <button type='sumbmit'>Push</button>
+          </form>
+          <ul>
+            {items}
+          </ul>
+        </div>
+      );
+    } else {
+      return <button onClick={dropboxLogin}>dropbox login</button>
+    }
   }
 });
 
